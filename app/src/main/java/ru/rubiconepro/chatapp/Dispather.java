fcbad12c7c0bdd23ba088eed35c612ec3be82c56
gusev71 <SERGE-GUSEV-007@yandex.ru>
2018-10-08 21:04:06 +0300

    custom application

---
+++ app/src/main/java/ru/rubiconepro/chatapp/Dispather.java
@@ -0,0 +1,6 @@
+package ru.rubiconepro.chatapp.Storage;
+
+import ru.rubiconepro.chatapp.Communicator.IResiver;
+
+public class Dispather implements IResiver {
+}
---
+++ app/src/main/java/ru/rubiconepro/chatapp/MyApp.java
@@ -0,0 +1,6 @@
+package ru.rubiconepro.chatapp;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+}
